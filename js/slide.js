bb1e2380cbdfbd1ac485dfca1611002b0b6920d9
Rafael Filho <57054037+rafaelofil@users.noreply.github.com>
2020-02-23 19:22:23 -0300

    Revert "final navegação prev e next"

--- js/slide.js
+++ js/slide.js
@@ -5,9 +5,6 @@ export default class Slide {
     this.dist = { finalPosition: 0, startX: 0, movement: 0 }
   }
 
-  transition(active) {
-    this.slide.style.transition = active ? 'transform .3s' : '';
-  }
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -29,7 +26,6 @@ export default class Slide {
       movetype = 'touchmove';
     }
     this.wrapper.addEventListener(movetype, this.onMove);
-    this.transition(false);
   }
 
   onMove(event) {
@@ -42,18 +38,6 @@ export default class Slide {
     const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
     this.wrapper.removeEventListener(movetype, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
-    this.transition(true);
-    this.changeSlideOnEnd();
-  }
-
-  changeSlideOnEnd() {
-    if (this.dist.movement > 120 && this.index.next !== undefined){
-      this.activeNextSlide();
-    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
-      this.activePrevSlide();
-    } else {
-      this.changeSlide(this.index.active);
-    }
   }
 
   addSlideEvents() {
@@ -69,6 +53,7 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
+  // Slides config
 
   slidePosition(slide) {
     const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
@@ -98,19 +83,8 @@ export default class Slide {
     this.dist.finalPosition = activeSlide.position;
   }
 
-  activePrevSlide() {
-    if (this.index.prev !== undefined)
-      this.changeSlide(this.index.prev);
-  }
-
-  activeNextSlide() {
-    if (this.index.next !== undefined)
-      this.changeSlide(this.index.next);
-  }
-
   init() {
     this.bindEvents();
-    this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
     return this;
